cd14a4065c193f0579a058871293618727cb9993
JoeSadler <joesadler@Joes-MBP-2.attlocal.net>
2015-06-08 15:47:31 -0400

    add additional data fields to UserData.java
    Added more data fields and getters and setters to the UserData.java
    file.

--- src/java/UserData.java
+++ src/java/UserData.java
@@ -1,3 +1,6 @@
+
+import java.io.Serializable;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,14 +16,40 @@ class UserData {
     private String password;
     private String email;
     private String address;
+    private String major;
+    private String additionalInfo;
     
     UserData(String nm, String ps) {
         name = nm;
         password = ps;
     }
     
+    void updateData(String em, String address, String major, String additionalInfo) {
+        this.email = em;
+        this.address = address;
+        this.major = major;
+        this.additionalInfo = additionalInfo;
+    }
+    
     boolean checkLogin(String p) {
         return p.equals(password);
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public String getMajor() {
+        return major;
+    }
+    
+    public String getAdditionalInfo() {
+        System.out.println(additionalInfo);
+        return additionalInfo;
     }
     
 }
