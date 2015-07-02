6042bff124f08ebb47ff569c25afb8f03ad9afcc
Timothy Lee-O'Connor <tleeoconnor@yahoo.com>
2015-07-02 16:04:43 -0400

    Merging

--- src/java/User.java
+++ src/java/User.java
@@ -164,6 +164,7 @@ public class User implements Serializable {
             return null;
         }
         System.out.println("Login Success");
+        userManager.saveData();
             return "home_new"; //"home"
     }
     
